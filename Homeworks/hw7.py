43495c1203fc132486cdbb7effdd8c325e0f7289
mrmattkennedy <kennemat@mail.gvsu.edu>
2018-11-30 22:59:42 -0500

    Added cache print statements for hw7

--- Homeworks/hw7.py
+++ Homeworks/hw7.py
@@ -1,22 +1,22 @@
 import math
 import sys
 
-########################
-#     Cache
-#    _______
-#   |___|___|}
-#   |___|___| } set size
-#   |___|___| }
-#   |___|___|}
-#       V
-#    way size
-########################
+###############################################
+#                   Cache
+#            _____________________
+#           |0xFFFFFFFF|0xFFFFFFFF|
+#           |0xFFFFFFFF|0xFFFFFFFF|
+#           |0xFFFFFFFF|0xFFFFFFFF|
+#           |0xFFFFFFFF|0xFFFFFFFF|
+#            ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+#   Capacity = block size * way size * set size
+###############################################
 
 totalBits = 32
 blocks = 8
 base = 2
 
-def nWay(binData, ways):
+def nWay(hexData, binData, ways):
     if blocks % ways != 0:
         print("Ways is not a valid value")
         return
@@ -32,7 +32,11 @@ def nWay(binData, ways):
 
     misses = 0
     hits = 0
+    count = 0
+    print("\n" + str(ways) + " ways:\n")
+
     for num in binData:
+        hitTag = ""
         #convert int to binary, shift right to get rid of set and 00, [2:] to remove 0b
         tag = bin(int(num, base) >> tag_bits_offset)[2:].zfill(tag_size)            
         #get rid of 00, take last set_bits_size nums
@@ -42,22 +46,47 @@ def nWay(binData, ways):
             set = 0
 
         if tag in cache[set]:
-            print(num + " at " + str(set) + " hit")                     #hit
+            hitTag = "hit"
             hits += 1 
         else:
-            print(num + " at " + str(set) + " miss")                    #miss
+            hitTag = "miss"
             lru = min(mostRecent[set])                                  #least recently used
             cache[set][lru] = tag
             mostRecent[set][lru] = ways
             mostRecent[set][:] = [num - 1 for num in mostRecent[set]]
-            misses += 1
-
+            misses += 1        
+        print("#" + str(count+1) + ": 0x" + hexData[count] + "/" + num + " at set " + bin(set)[2:].zfill(set_bits_size) + "/" + str(set) + " " + hitTag)
+        count += 1
+        for elem in cache:
+            print(elem)
+        print("\n")
+    
     print("Hits: " + str(hits))
     print("Misses: " + str(misses))
-    for elem in cache:
-        print(elem)
 
-hexdata = ["F000CA7C",
+                          
+def getData(hexData, binData):
+    ways = [1, 2, 4]
+    count = 0
+    for num in binData:
+        temp = ""
+        for way in ways:
+            num_sets = int(blocks / way)                #number of sets = # blocks / # ways
+            set_bits_size = int(math.log(num_sets, 2))  #set bit size = log base 2 (number of sets)
+            tag_bits_offset = set_bits_size + 2         #00 + set bits
+            tag_size = totalBits - tag_bits_offset      #number of bits in tag = 32 - set bits - 2 (for 00)
+            
+            #convert int to binary, shift right to get rid of set and 00, [2:] to remove 0b
+            tag = bin(int(num, base) >> tag_bits_offset)[2:].zfill(tag_size)         
+            #get rid of 00, take last set_bits_size nums
+            set = (bin(int(num, base) >> 2))[-set_bits_size:].zfill(set_bits_size)
+
+            temp += "#Ways: " + str(way) + "\tSet-" + str(set) + "\tTag-" + str(tag) + "\n"
+
+        print("#" + str(count+1) + ": 0x" + hexData[count] + " => " + num + "\n" + temp + "\n")
+        count += 1
+        
+hexData = ["F000CA7C",
         "110092AC",
         "AB20CAE8",
         "12A0CA94",
@@ -79,12 +108,12 @@ hexdata = ["F000CA7C",
 binData = []
 scale = 16 #hex
 
-for num in hexdata:
+for num in hexData:
     temp = bin(int(num, scale))[2:].zfill(totalBits) #fill leading zeros
-    print(num + " => " + temp)
     binData.append(temp)
+
 
 print("\n\n")
 for ways in sys.argv[1:]:
-       nWay(binData, int(ways))
-
+       nWay(hexData, binData, int(ways))
+#getData(hexData, binData)
